3bcc2459d1eccd3e4d33341796114b3b18128353
Milind Agarwal <agarwalmilind77@gmail.com>
2026-06-19 23:01:03 +0530

    added render backend url

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,5 +1,5 @@
-export const USER_API_ENDPOINT = "http://localhost:3000/api/users";
-export const JOB_API_ENDPOINT = "http://localhost:3000/api/job";
-export const APPLICATION_API_ENDPOINT = "http://localhost:3000/api/application"
-export const COMPANY_API_ENDPOINT = "http://localhost:3000/api/company"
-export const AI_API_ENDPOINT = "http://localhost:3000/api/ai"
+export const USER_API_ENDPOINT = "https://jobportal-b-8qo9.onrender.com/users";
+export const JOB_API_ENDPOINT = "https://jobportal-b-8qo9.onrender.com/job";
+export const APPLICATION_API_ENDPOINT = "https://jobportal-b-8qo9.onrender.com/application"
+export const COMPANY_API_ENDPOINT = "https://jobportal-b-8qo9.onrender.com/company"
+export const AI_API_ENDPOINT = "https://jobportal-b-8qo9.onrender.com/ai"
