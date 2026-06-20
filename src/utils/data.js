ad2775eba93a0582f5beae31a08ed037ec15ebc1
Milind Agarwal <agarwalmilind77@gmail.com>
2026-06-20 11:28:03 +0530

    updated data.js

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,5 +1,5 @@
-export const USER_API_ENDPOINT = "https://jobportal-b-8qo9.onrender.com/users";
-export const JOB_API_ENDPOINT = "https://jobportal-b-8qo9.onrender.com/job";
-export const APPLICATION_API_ENDPOINT = "https://jobportal-b-8qo9.onrender.com/application"
-export const COMPANY_API_ENDPOINT = "https://jobportal-b-8qo9.onrender.com/company"
-export const AI_API_ENDPOINT = "https://jobportal-b-8qo9.onrender.com/ai"
+export const USER_API_ENDPOINT = "https://jobportal-b-8qo9.onrender.com/api/users";
+export const JOB_API_ENDPOINT = "https://jobportal-b-8qo9.onrender.com/api/job";
+export const APPLICATION_API_ENDPOINT = "https://jobportal-b-8qo9.onrender.com/api/application";
+export const COMPANY_API_ENDPOINT = "https://jobportal-b-8qo9.onrender.com/api/company";
+export const AI_API_ENDPOINT = "https://jobportal-b-8qo9.onrender.com/api/ai";
